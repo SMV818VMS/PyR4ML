11d505b5d6e4a169cd5881b28fda9e2e0b8f80f4
Samuel Miravet Verde <samuelmiver@gmail.com>
2016-03-21 19:19:18 +0100

    RF

--- RandomForestClassifier.py
+++ RandomForestClassifier.py
@@ -56,9 +56,65 @@ savetxt('Data/classes_predicted.csv', rf.predict(test), delimiter=',', fmt='%f')
 savetxt('Data/probabil_predicted.csv', rf.predict_proba(test), delimiter=',', fmt='%f')
 
 
+# In[90]:
+
+l = ['strand', 'bidirectional', 'stem_len', 'loop_len', 'spacer_len','energy', 'mismatched_b', 'GC', 'Ts', 'ptsc1', 'ptsc2', 'distbefdistaft']
+
+for i in range(0, 13):
+    print(l[i]+' = '+ str(rf.feature_importances_[i]))
+    
+print(rf.estimators)
+
+# std = np.std([rf.feature_importances_ for tree in rf.estimators_], axis=0)
+
+
 # In[ ]:
 
+### Plotting feature importance
 
+
+# In[82]:
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_classification
+from sklearn.ensemble import ExtraTreesClassifier
+
+# Build a classification task using 3 informative features
+X, y = make_classification(n_samples=1000,
+                           n_features=10,
+                           n_informative=3,
+                           n_redundant=0,
+                           n_repeated=0,
+                           n_classes=2,
+                           random_state=0,
+                           shuffle=False)
+
+# Build a forest and compute the feature importances
+forest = ExtraTreesClassifier(n_estimators=250,
+                              random_state=0)
+
+forest.fit(X, y)
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+             axis=0)
+indices = np.argsort(importances)[::-1]
+
+# Print the feature ranking
+print("Feature ranking:")
+
+for f in range(X.shape[1]):
+    print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+
+# Plot the feature importances of the forest
+plt.figure()
+plt.title("Feature importances")
+plt.bar(range(X.shape[1]), importances[indices],
+       color="r", yerr=std[indices], align="center")
+plt.xticks(range(X.shape[1]), indices)
+plt.xlim([-1, X.shape[1]])
+plt.show()
 
 
 # In[ ]:
